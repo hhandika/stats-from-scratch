2ec1d357ef1a0cbea45e4300f82d78ec7dd8e729
Heru Handika <herubiolog@gmail.com>
2020-09-21 20:30:32 -0500

    Add rust implementation for counting mean

--- linear-algebra/vectors/vector.rs
+++ linear-algebra/vectors/vector.rs
@@ -13,6 +13,11 @@ fn main() {
 
     println!("Vector multiplication: {:?}", 
         multiply_two_vectors(&x, &y));
+    
+    println!("Sum x: {}", sum_vectors(&x));
+    println!("Sum y: {}", sum_vectors(&y));
+    println!("Mean x: {:.2}", mean_vectors(&x));
+    println!("Mean y: {:.2}", mean_vectors(&y));
 }
 
 fn add_two_vectors(x: &Vec<i32>, y: &Vec<i32>) -> Vec<i32>{
@@ -31,4 +36,19 @@ fn multiply_two_vectors(x: &Vec<i32>, y: &Vec<i32>) -> Vec<i32>{
         result.push(i * j);
     }
     result
+}
+
+fn sum_vectors(x: &Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in x.iter() {
+        sum += i
+    }
+    sum
+}
+
+fn mean_vectors(vec: &Vec<i32>) -> f64 {
+    let n = vec.len();
+    let mean = sum_vectors(vec) as f64 / n as f64;
+
+    mean
 }
