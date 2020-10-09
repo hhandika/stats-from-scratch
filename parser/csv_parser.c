59291c560588eed0899a5d8bbd6d9ff47107c141
Heru Handika <herubiolog@gmail.com>
2020-10-09 12:41:36 -0500

    Update csv parser

--- parser/csv_parser.c
+++ parser/csv_parser.c
@@ -7,8 +7,21 @@ Work on parsing csv file
 #include <stdio.h>
 #include <stdlib.h>
 
+int main (void) {
+    int ch;
+    FILE * fp;
+    char * filename = "data/tissue.csv";
+    
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Failed to open the file\n");
+        exit(EXIT_FAILURE);
+    }
 
-typedef struct {
-    char * field_number;
-    char * 
+
+    while ((ch = getc(fp)) != EOF) {
+        printf("%c", ch);
+    }
+    return EXIT_SUCCESS;
 }
+
