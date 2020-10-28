77ea76c5ae596d16c2056f05d660e6230a90d3e6
Heru Handika <herubiolog@gmail.com>
2020-10-28 14:37:47 -0500

    Remove min and max functions

--- 2_central-tendency/ctd.c
+++ 2_central-tendency/ctd.c
@@ -12,8 +12,6 @@ Central tendency
 void print_vector(int * vec, size_t vec_size);
 double sum_vector(int * vec, size_t vec_size);
 double mean_vector(int * vec, size_t vec_size);
-int max_vector(int * vec, size_t vec_size);
-int min_vector(int * vec, size_t vec_size);
 
 int main(void) {
     int vec[N] = {10, 20, 23, 24, 25, 
@@ -21,10 +19,6 @@ int main(void) {
 
     printf("Vector values: ");
     print_vector(vec, N);
-
-    printf("Mean vector %.2f\n", mean_vector(vec, N));
-    printf("Max values: %d\n", max_vector(vec, N));
-    printf("Min values: %d\n", min_vector(vec, N));
 
     return EXIT_SUCCESS;
 }
@@ -49,22 +43,3 @@ double mean_vector(int * vec, size_t vec_size) {
     return sum_vector(vec, vec_size) / vec_size;
 }
 
-int max_vector(int * vec, size_t vec_size) {
-    int max = 0;
-    size_t i = 0;
-    while (i < vec_size) {
-        if (vec[i] > max) max = vec[i];
-        i++;
-    }
-    return max;
-}
-
-int min_vector(int * vec, size_t vec_size) {
-    int min = vec[0];
-    size_t i = 1;
-    while (i < vec_size) {
-        if (vec[i] < min) min = vec[i];
-        i++;
-    }
-    return min;
-}
