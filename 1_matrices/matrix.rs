2b59f03abcf25b0d5a1f7e7d4031a71981f1c94e
Heru Handika <herubiolog@gmail.com>
2020-10-30 19:39:27 -0500

    Add identity matrix in rust

--- 1_matrices/matrix.rs
+++ 1_matrices/matrix.rs
@@ -3,10 +3,28 @@
 //! Matrix
 
 fn main() {
-    println!("{:?}", vec);
+    const ROWS: usize = 6;
+    const COLS: usize = 6;
+    let mat = create_identity_matrix(ROWS, COLS);
+    print_matrix(mat);
 }
 
-fn create_identity_matrix() -> i32 {
-    let mut vec = [[i32; 4]; 4]
-    vec
+fn print_matrix(mat: Vec<Vec<i32>>) {
+    for i in mat.iter() {
+        println!("{:?}", i);
+    }
+}
+
+fn create_identity_matrix(row_size: usize, col_size: usize) -> Vec<Vec<i32>>{
+    let mut mat = vec![Vec::with_capacity(row_size); col_size];
+    for i in 0..mat.len() {
+        for j in 0..mat.len() {
+            if i == j {
+                mat[i].push(1);
+            } else {
+                mat[i].push(0);
+            }
+        }
+    }
+   mat
 }
