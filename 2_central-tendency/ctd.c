31bd31df9973f83606d50c894e3c944490f9a3dc
Heru Handika <herubiolog@gmail.com>
2020-11-08 11:31:33 -0600

    Change implementation to double and add median

--- 2_central-tendency/ctd.c
+++ 2_central-tendency/ctd.c
@@ -9,29 +9,43 @@ Central tendency
 
 #define N 10
 
-void print_vector(int * vec, size_t vec_size);
-double sum_vector(int * vec, size_t vec_size);
-double mean_vector(int * vec, size_t vec_size);
+void print_vector(double * vec, size_t vec_size);
+double sum_vector(double * vec, size_t vec_size);
+double mean_vector(double * vec, size_t vec_size);
+void swap(double * arr_i, double * arr_j);
+void sort_vector(double * vec, size_t vec_size);
+double find_median(double * vec, size_t vec_size);
 
 int main(void) {
-    int vec[N] = {10, 20, 23, 24, 25, 
-                26, 27, 28, 29, 30};
+    double vec[N] = {10.4, 28.2, 23.3, 23.2, 20.1, 
+                26.1, 27.2, 18.2, 29.1, 30.0};
 
+    double vec_dob[] = {13.2, 13.5, 10.3, 12.0,
+                        20.1, 50.3, 15.4};
+
+    sort_vector(vec, N);
     printf("Vector values: ");
     print_vector(vec, N);
+
+    size_t vec_size = sizeof(vec_dob)/sizeof(vec_dob[0]);
+    puts("Vector double: ");
+    sort_vector(vec_dob, vec_size);
+    print_vector(vec_dob, vec_size);
+    
+    printf("Median: %.2f\n", find_median(vec_dob, vec_size));
 
     printf("Mean vector %.2f\n", mean_vector(vec, N));
 
     return EXIT_SUCCESS;
 }
 
-void print_vector(int * vec, size_t vec_size) {
+void print_vector(double * vec, size_t vec_size) {
     for (size_t i = 0; i < vec_size; i++)
-        printf("%d ", vec[i]);
+        printf("%.2f ", vec[i]);
     printf("\n");
 }
 
-double sum_vector(int * vec, size_t vec_size) {
+double sum_vector(double * vec, size_t vec_size) {
     double sum = 0.0;
     size_t i = 0;
     while (i < vec_size) {
@@ -41,7 +55,36 @@ double sum_vector(int * vec, size_t vec_size) {
     return sum;
 }
 
-double mean_vector(int * vec, size_t vec_size) {
+// Swap for sorting arrays
+void swap(double * arr_i, double * arr_j) {
+    double temp = *arr_i;
+    *arr_i = *arr_j;
+    *arr_j = temp;
+}
+
+// Using bubble sort to sort vector values
+void sort_vector(double * vec, size_t vec_size) {
+    for (size_t step = 0; step < vec_size - 1; step++) {
+        for (size_t i = 0; i < vec_size - step - 1; i++) {
+            if (vec[i] > vec[i + 1]) {
+                swap(&vec[i], &vec[i + 1]);
+            }
+        }
+    }
+}
+
+double mean_vector(double * vec, size_t vec_size) {
     return sum_vector(vec, vec_size) / vec_size;
 }
 
+double find_median(double * vec, size_t vec_size) {
+    sort_vector(vec, vec_size);
+    // Get mid point
+    size_t hi_midpoint = vec_size / 2;
+    // If even
+    if (vec_size % 2 == 0) {
+        return (double)(vec[hi_midpoint - 1] + vec[hi_midpoint]) / 2.0;
+    }
+    // if odd
+    return (double) vec[hi_midpoint];
+}
