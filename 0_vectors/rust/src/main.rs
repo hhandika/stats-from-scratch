7ee61b91825b06bb4293aa7e5d638223a1c3c45a
Heru Handika <herubiolog@gmail.com>
2020-12-16 18:40:11 -0600

    Shorten the module path in the main

--- 0_vectors/rust/src/main.rs
+++ 0_vectors/rust/src/main.rs
@@ -3,34 +3,35 @@ Heru Handika
 19 September 2020
 Vector addition and multiplication in rust
 */
-
 mod linear_algebra;
+
+use linear_algebra::vector as vec;
 
 fn main() {
     let x: Vec<i32> = vec![1, 2, 3, 4, 5];
     let y: Vec<i32> = vec![10, 20, 30, 40, 50];
 
     println!("Vector ones: {:?}",
-        linear_algebra::vector::vector_ones(5));
+        vec::vector_ones(5));
 
     println!("Vector addition: {:?}", 
-        linear_algebra::vector::add_two_vectors(&x, &y));
+        vec::add_two_vectors(&x, &y));
 
     println!("Vector substraction: {:?}",
-        linear_algebra::vector::substract_two_vectors(&x, &y));
+        vec::substract_two_vectors(&x, &y));
 
     println!("Vector multiplication: {:?}", 
-        linear_algebra::vector::multiply_two_vectors(&x, &y));
+        vec::multiply_two_vectors(&x, &y));
     
-    println!("Sum x: {}", linear_algebra::vector::sum_vectors(&x));
-    println!("Sum y: {}", linear_algebra::vector::sum_vectors(&y));
-    println!("Mean x: {:.2}", linear_algebra::vector::mean_vectors(&x));
-    println!("Mean y: {:.2}", linear_algebra::vector::mean_vectors(&y));
+    println!("Sum x: {}", vec::sum_vectors(&x));
+    println!("Sum y: {}", vec::sum_vectors(&y));
+    println!("Mean x: {:.2}", vec::mean_vectors(&x));
+    println!("Mean y: {:.2}", vec::mean_vectors(&y));
 
     println!("Sum of square x: {:.2}", 
-        linear_algebra::vector::calculate_sum_of_square(&x));
+        vec::calculate_sum_of_square(&x));
     println!("Sum of square y: {:.2}", 
-        linear_algebra::vector::calculate_sum_of_square(&y));
+        vec::calculate_sum_of_square(&y));
     println!("X & Y distance: {:.4}", 
-        linear_algebra::vector::compute_vector_distance(&x, &y));
+        vec::compute_vector_distance(&x, &y));
 }
