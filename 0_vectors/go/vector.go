a22b63b28c0d6a1cca8c37fd147cf939c5794086
Heru Handika <herubiolog@gmail.com>
2021-02-26 17:58:23 -0600

    Update go vector operation.

--- 0_vectors/go/vector.go
+++ 0_vectors/go/vector.go
@@ -6,24 +6,94 @@ Vector operation in go
 
 package main
 
-import (
-	"fmt"
-)
+import ("fmt")
 
 func main() {
-	x := [5]int32{1, 2, 3, 4, 5}
-	y := [5]int32{10, 20, 30, 40, 50}
+	x := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
+	y := []float64{10.0, 20.0, 30.0, 40.0, 50.0}
 
-	sumX := sumVector(&x)
-	sumY := sumVector(&y)
-
-	fmt.Printf("Sum vec x: %d and y: %d\n", sumX, sumY)
+	fmt.Println("Vec ones: ", ones(5))
+	fmt.Printf("Sum vec x: %.2f and y: %.2f\n", sum(x), sum(y))
+	fmt.Println("Mean x: ", mean(x))
+	fmt.Println("Vec addition: ", add(x, y))
+	fmt.Println("Vec subraction: ", substract(x, y))
+	fmt.Println("Vec multiplication: ", multiply(x, y))
+	fmt.Println("Cumsum: ", cumsum(x))
 }
 
-func sumVector(vec *[5]int32) int32 {
-	var sum int32 = 0
-	for _, value := range vec {
-		sum += value
+func ones(size uint32) []float64 {
+	res := make([]float64, size)
+
+	for i := range res {
+		res[i] = 1.0
+	}
+
+	return res
+}
+
+func add(x []float64, y []float64) []float64 {
+	if len(x) != len(y) {
+		panic("UNEQUAL VECTOR LENGTH")
+	}
+
+	res := make([]float64, len(x))
+
+	for i := range x {
+		res[i] = x[i] + y[i]
+	}
+	return res
+}
+
+func substract(x []float64, y []float64) []float64 {
+	if len(x) != len(y) {
+		panic("UNEQUAL VECTOR LENGTH")
+	}
+
+	res := make([]float64, len(x))
+
+	for i := range x {
+		res[i] = x[i] - y[i]
+	}
+
+	return res
+}
+
+func multiply(x []float64, y []float64) []float64 {
+	if len(x) != len(y) {
+		panic("UNEQUAL VECTOR LENGTH")
+	}
+
+	res := make([]float64, len(x))
+
+	for i := range x {
+		res[i] = x[i] * y[i]
+	}
+
+	return res
+}
+
+func cumsum(vec []float64) []float64 {
+	res := make([]float64, len(vec))
+	
+	sum := 0.0
+	for i, val := range vec {
+		sum += val
+		res[i] = sum
+	}
+
+	return res
+}
+
+
+func sum(vec []float64) float64 {
+	var sum float64 = 0
+	for _, val := range vec {
+		sum += val
 	}
 	return sum
+}
+
+
+func mean(vec []float64) float64 {
+	return sum(vec) / float64(len(vec))
 }
