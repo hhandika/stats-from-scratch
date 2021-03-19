eff019e966a63294045753b45b2de2b46a22694a
Heru Handika <herubiolog@gmail.com>
2021-03-19 17:31:13 -0500

    Move functions to a separate file.

---
+++ 0_vectors/go/main.go
@@ -0,0 +1,24 @@
+/*
+Heru Handika
+27 October 2020
+Vector operation in go
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
+	y := []float64{10.0, 20.0, 30.0, 40.0, 50.0}
+
+	fmt.Println("Vec ones: ", Ones(5))
+	fmt.Printf("Sum vec x: %.2f and y: %.2f\n", Sum(x), Sum(y))
+	fmt.Println("Mean x: ", Mean(x))
+	fmt.Println("Vec addition: ", Add(x, y))
+	fmt.Println("Vec subraction: ", Substract(x, y))
+	fmt.Println("Vec multiplication: ", Multiply(x, y))
+	fmt.Println("Cumsum: ", Cumsum(x))
+}
--- 0_vectors/go/vector.go
+++ 0_vectors/go/vector.go
@@ -1,27 +1,6 @@
-/*
-Heru Handika
-27 October 2020
-Vector operation in go
-*/
-
 package main
 
-import ("fmt")
-
-func main() {
-	x := []float64{1.0, 2.0, 3.0, 4.0, 5.0}
-	y := []float64{10.0, 20.0, 30.0, 40.0, 50.0}
-
-	fmt.Println("Vec ones: ", ones(5))
-	fmt.Printf("Sum vec x: %.2f and y: %.2f\n", sum(x), sum(y))
-	fmt.Println("Mean x: ", mean(x))
-	fmt.Println("Vec addition: ", add(x, y))
-	fmt.Println("Vec subraction: ", substract(x, y))
-	fmt.Println("Vec multiplication: ", multiply(x, y))
-	fmt.Println("Cumsum: ", cumsum(x))
-}
-
-func ones(size uint32) []float64 {
+func Ones(size uint32) []float64 {
 	res := make([]float64, size)
 
 	for i := range res {
@@ -31,7 +10,7 @@ func ones(size uint32) []float64 {
 	return res
 }
 
-func add(x []float64, y []float64) []float64 {
+func Add(x []float64, y []float64) []float64 {
 	if len(x) != len(y) {
 		panic("UNEQUAL VECTOR LENGTH")
 	}
@@ -44,7 +23,7 @@ func add(x []float64, y []float64) []float64 {
 	return res
 }
 
-func substract(x []float64, y []float64) []float64 {
+func Substract(x []float64, y []float64) []float64 {
 	if len(x) != len(y) {
 		panic("UNEQUAL VECTOR LENGTH")
 	}
@@ -58,7 +37,7 @@ func substract(x []float64, y []float64) []float64 {
 	return res
 }
 
-func multiply(x []float64, y []float64) []float64 {
+func Multiply(x []float64, y []float64) []float64 {
 	if len(x) != len(y) {
 		panic("UNEQUAL VECTOR LENGTH")
 	}
@@ -72,9 +51,9 @@ func multiply(x []float64, y []float64) []float64 {
 	return res
 }
 
-func cumsum(vec []float64) []float64 {
+func Cumsum(vec []float64) []float64 {
 	res := make([]float64, len(vec))
-	
+
 	sum := 0.0
 	for i, val := range vec {
 		sum += val
@@ -84,8 +63,7 @@ func cumsum(vec []float64) []float64 {
 	return res
 }
 
-
-func sum(vec []float64) float64 {
+func Sum(vec []float64) float64 {
 	var sum float64 = 0
 	for _, val := range vec {
 		sum += val
@@ -93,7 +71,6 @@ func sum(vec []float64) float64 {
 	return sum
 }
 
-
-func mean(vec []float64) float64 {
-	return sum(vec) / float64(len(vec))
+func Mean(vec []float64) float64 {
+	return Sum(vec) / float64(len(vec))
 }
